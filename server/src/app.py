5bd6fa0b4ecc226a9867fc1ccc19ade95410e1bf
Pablo <valdunciel98@gmail.com>
2021-09-08 12:23:22 +0200

    update g-rel mapping of labels

--- server/src/app.py
+++ server/src/app.py
@@ -1,17 +1,17 @@
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from flask_restful import Api
-from resources.user import UserList
-from resources.document import Document, DocumentList
+from resources.user import UserListResource
+from resources.document import DocumentResource, DocumentListResource
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)  # allow CORS
 
 # --- API calls ---
-api.add_resource(UserList, '/api/user/ids')
-api.add_resource(DocumentList, '/api/document/ids')
-api.add_resource(Document, '/api/document/<string:user_id>/<string:doc_id>')
+api.add_resource(UserListResource, '/api/user/ids')
+api.add_resource(DocumentListResource, '/api/document/ids')
+api.add_resource(DocumentResource, '/api/document/<string:user_id>/<string:doc_id>')
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True)
--- server/src/config.py
+++ server/src/config.py
@@ -11,3 +11,8 @@ DATA_DIR = pjoin(DIR, 'data')
 GAZE_DIR = pjoin(DATA_DIR, 'gaze')
 ARTICLES_DIR = pjoin(DATA_DIR, 'articles')
 MAPPING_DIR = pjoin(DATA_DIR, 'mapping')
+
+# Study dimensions
+STUDY_WIDTH = 2560
+STUDY_HEIGHT = 1440
+
