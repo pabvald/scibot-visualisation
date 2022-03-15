2890b61fbc90c6fa16b949465fa50ab13c03c495
Pablo <valdunciel98@gmail.com>
2022-03-15 11:42:38 +0100

    add src directory to path

--- server/src/app.py
+++ server/src/app.py
@@ -1,3 +1,11 @@
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
 from flask import Flask, render_template
 from flask_cors import CORS
 from flask_restful import Api
